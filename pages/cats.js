6fbe86f9b4ba1600e4c220cddead5a5d65c919f1
Vinicius <viniciuslimavi05@gmail.com>
2023-12-04 18:14:35 -0300

    primeiro commit

---
+++ pages/cats.js
@@ -0,0 +1,11 @@
+// pages/index.js
+
+export default function Home() {
+    return (
+      <div>
+        <h1>Sobre Cachorros</h1>
+        <p>Os gatos são animais de estimação adoráveis e leais. Eles são conhecidos como os melhores amigos do homem e vêm em uma grande variedade de raças, tamanhos e personalidades.</p>
+      </div>
+    )
+  }
+  
